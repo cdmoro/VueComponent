2d2602b8b11d9fe68cd28c42627701272666f2a2
Carlos Bonadeo <carlosbonadeo@gmail.com>
2018-01-22 21:30:10 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-	<div id="app">
-		<img width=200 src="./assets/logo.png">
+	<b-container id="app">
 		<router-view></router-view>
-	</div>
+	</b-container>
 </template>
 
 <script>
@@ -16,8 +15,7 @@
 		font-family: 'Avenir', Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
 		color: #2c3e50;
-		margin-top: 60px;
+		margin-top: 20px;
 	}
 </style>
